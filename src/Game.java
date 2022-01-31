db285b457236b3b634d5657c23a988650cf50acc
natistuomi <77719270+natistuomi@users.noreply.github.com>
2022-01-31 10:16:50 +0100

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,133 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Game {
+    public static void main (String[] args) {
+        String word = pickWord();
+        int tries = 12;
+        String dashes = makeDashes(word);
+        char letter;
+        String guessed = " ";
+        for(int i = 12; i > 0; i--){
+            letter = getLetter(dashes, tries).charAt(0);
+            if(guessedLetter(letter, guessed)){
+                System.out.println("You've already guessed " + letter);
+                i += 1;
+            }
+            else{
+                guessed = guessed + letter;
+                tries = i;
+                if(wordIncludesLetter(word, letter)){
+                    dashes = dashConversion(word, dashes, letter);
+                }
+            }
+            if(gameWon(dashes)){
+                i = 0;
+            }
+        }
+        if(gameWon(dashes)){
+            System.out.println(wordFound(dashes, tries));
+        }
+        else{
+            System.out.println(gameLost(dashes, word));
+        }
+    }
+
+    public static String pickWord(){
+        System.out.print("What's your word? ");
+        Scanner tgb = new Scanner(System.in);
+        return tgb.nextLine().toUpperCase();
+    }
+
+    public static String makeDashes(String w){
+        String dashes = "_ ";
+        for(int i = 0; i < w.length()-1; i++){
+            dashes = dashes + "_ ";
+        }
+        return dashes;
+    }
+
+    public static String getLetter(String s, int a){
+        while(true){
+            System.out.println(s + "(" + a + " tries left)");
+            System.out.print("Pick a letter: ");
+            Scanner tgb = new Scanner(System.in);
+            String letter = tgb.nextLine();
+            System.out.println(" ");
+            letter = letter.toUpperCase();
+            if (letter.length() == 1 && checkIfLetter(letter)) {
+                return letter;
+            } else {
+                System.out.println("Not valid letter");
+            }
+        }
+    }
+
+    public static boolean checkIfLetter(String s){
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for(int i = 0; i < alphabet.length(); i++){
+            if(s.charAt(0) == alphabet.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean guessedLetter(char l, String s){
+        for(int i = 0; i < s.length(); i++){
+            if(l == s.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean wordIncludesLetter(String s, char l){
+        for(int i = 0; i < s.length(); i++){
+            if(l == s.charAt(i)){
+                System.out.println("Well done!");
+                return true;
+            }
+        }
+        System.out.println("Oh no!");
+        return false;
+    }
+
+    public static String dashConversion(String w, String s, char l){
+        String newdashes = "";
+        for(int i = 0; i < w.length(); i++){
+            if(w.charAt(i) == l){
+                newdashes = newdashes + l + " ";
+            }
+            else if(s.charAt(i*2) != ' ' || s.charAt(i*2) != '_'){
+                newdashes = newdashes + s.charAt(i*2) + " ";
+            }
+            else{
+                newdashes = newdashes + "_ ";
+            }
+        }
+        return newdashes;
+    }
+
+    public static boolean gameWon(String s){
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '_'){
+                return false;
+            }
+            i += 1;
+        }
+        return true;
+    }
+
+    public static String wordFound(String s, int a){
+        System.out.println("You won!");
+        return s + "(You had " + a + " tries left)";
+    }
+
+    public static String gameLost(String s, String w){
+        System.out.println(s);
+        return "You lost! The word was " + w;
+    }
+
+}
+
